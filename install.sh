a7472198f41c6e67badd67e53e8e07ff223622aa
chparmley <63277749+chparmley@users.noreply.github.com>
2021-07-08 03:26:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,7 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo reboot now
 sudo apt update
-sudo apt install realtek-rtl88xxau-dkms
-sudo apt install dkms
 git clone https://github.com/aircrack-ng/rtl8812au
 cd rtl8812au/
 sudo make
 sudo make install
-lsusb
-iwconfig
